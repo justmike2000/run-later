f23f3b8c9306b7d4add93b05fb9cec07d7501184
justmike2000 <justmike2000@gmail.com>
2017-01-22 17:30:11 -0600

    Extend admin

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,11 @@ class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organization = models.ForeignKey("Organization")
 
+    def __unicode__(self):
+        return "{} - {} {}".format(self.id, self.user, self.organization)
+
 class Organization(models.Model):
     description = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "{} - {}".format(self.id, self.description)
