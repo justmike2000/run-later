1cc6263711ea8288ea984b4632e4cf650ad70c64
justmike2000 <justmike2000@gmail.com>
2017-01-21 23:42:28 -0600

    update server

--- servers/management/commands/scheduler.py
+++ servers/management/commands/scheduler.py
@@ -12,30 +12,36 @@ class Command(BaseCommand):
         pass
 
     def run_job(self, command):
-        stdin, stdout, stderr  = self.ssh.exec_command("ls -al")
-        return stdin, stdout, stderr
+        return 0, "Output"
+        #stdin, stdout, stderr  = self.ssh.exec_command("ls -al")
+        #return stdin, stdout, stderr
+
+    def connect(self):
+        username = ""
+        password = ""
+
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect('localhost', username=username,
+                         password=password)
 
     def handle(self, *args, **kwargs):
-       username = ""
-       password = ""
+        self.connect()
 
-       self.ssh = paramiko.SSHClient()
-       self.ssh.set_missing_host_key_policy(
-                 paramiko.AutoAddPolicy())
-       self.ssh.connect('localhost', username=username,
-                         password=password)
-       while 1:
+        while 1:
+            pass
+        while 1:
            jobs = Job.objects.all()
 
            for job in jobs:
-               stdin, stdout, stderr = self.run_job(job.command)
-               data = stdout.read()
+               return_code, output = self.run_job(job.command)
 
                run = Run.objects.create(description=job.description,
                                         action=job.action,
                                         command=job.command,
                                         parameters=job.parameters,
                                         path=job.path,
+                                        return_code=eturn_code,
                                         result=data)
                print run.created_at, ":", job.command
 
