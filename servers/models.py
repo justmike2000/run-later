c966de1146ea507e2a005db5f2d7cf1c55de0af1
justmike2000 <justmike2000@gmail.com>
2017-01-21 23:34:37 -0600

    added more to server

--- servers/models.py
+++ servers/models.py
@@ -4,9 +4,17 @@ from django.db import models
 class Server(models.Model):
     hostname = models.CharField(max_length=255, blank=False, null=False)
     credential = models.ForeignKey("Credential")
-    pid = models.CharField(max_length=255)
+    pid = models.CharField(max_length=255, blank=True, null=True)
+
+    def __unicode__(self):
+        return "{} - {} {} {}".format(self.id, self.hostname,
+                                      self.credential.username, self.pid)
+
 
 class Credential(models.Model):
     username = models.CharField(max_length=255, blank=False, null=False)
     password = models.CharField(max_length=255, blank=False, null=False)
+
+    def __unicode__(self):
+        return "{} - {}".format(self.id, self.username)
 
