4d652e1898d0fab172d597f4ed958e389c45d38a
justmike2000 <justmike2000@gmail.com>
2017-01-22 18:20:00 -0600

    continue refator

--- servers/management/commands/scheduler.py
+++ servers/management/commands/scheduler.py
@@ -12,18 +12,27 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('server_id', nargs='+', type=int)
 
-    def run_job(self, command):
-        chan = self.ssh.get_transport().open_session()
+    def run_job(self, schedule):
+        client = self.connect(schedule.server.credential.username,
+                              schedule.server.credential.password,
+                              schedule.server.hostname)
+        command = schedule.job.command
+
+        chan = client.get_transport().open_session()
         chan.exec_command(command)
         return_code = chan.recv_exit_status()
         print command, return_code
 
     def connect(self, username, password, host):
-        self.ssh = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh.connect(host,
-                         username=username,
-                         password=password)
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(host,
+                           username=username,
+                           password=password)
+            return client
+        except paramiko.ssh_exception.BadAuthenticationType:
+            raise Exception("Invalid Credentials {}".format(server_id))
 
     def log_job(self, job):
         log = Run.objects.create(description=job.description,
@@ -43,18 +52,11 @@ class Command(BaseCommand):
         except Server.DoesNotExist:
             raise Exception("Server Not Defined {}".format(server_id))
 
-        try:
-            self.connect(server.credential.username,
-                         server.credential.password,
-                         server.hostname)
-        except paramiko.ssh_exception.BadAuthenticationType:
-            raise Exception("Invalid Credentials {}".format(server_id))
-
         while 1:
             schedules = Schedule.objects.filter(server=server)
 
             for schedule in schedules:
-                self.run_job(schedule.job.command)
+                self.run_job(schedule)
 
                 #log = self.log_job(schedule.job)
                 #print log.created_at, ":", schedule.job.command
