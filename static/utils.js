3bbd58841e530fabf39d5f78cfaed4632e18ee87
justmike2000 <justmike2000@gmail.com>
2017-01-28 19:43:32 -0600

    delete working

--- static/utils.js
+++ static/utils.js
@@ -70,3 +70,20 @@ function load_jobs(params, current_page) {
 function job_details(job) {
     window.location.href = '/dashboard/jobs/' + job + '/';
 }
+
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
